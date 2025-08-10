d5a0835695841531c53ddd0dc13a4b73f73b82e6
FranciscoVega <pancho.vega.90@gmail.com>
2025-08-09 21:36:36 -0400

    Commit inicial

---
+++ clase1.py
@@ -0,0 +1,12 @@
+#Ejercicio1
+# o. Elaborar el código para el cálculo de la raíz cuadrada de un número
+# • Restricción: no se puede usar sqrt de la biblioteca de Python.
+# • Debe solicitar el número por la entrada estándar e imprimir el resultado
+
+""" Ejercicio1
+x = float(input("Ingrese el numero al que desea calcular su raiz cuadrada : "))
+x_root = x ** (1/2)
+
+print(f"La raiz del numero ingresado {x} es : {x_root}")
+"""
+
